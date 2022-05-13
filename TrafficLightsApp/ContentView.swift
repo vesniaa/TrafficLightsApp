363b7cf80ba88177915bf4170817460f9ce94f43
Eugenia <pyssiki@yandex.ru>
2022-05-13 16:49:49 +0500

    color button and start button

--- TrafficLightsApp/ContentView.swift
+++ TrafficLightsApp/ContentView.swift
@@ -6,11 +6,61 @@
 //
 
 import SwiftUI
+enum CurrentLight {
+    case red, yellow, green
+}
 
 struct ContentView: View {
+    
+    @State private var buttonTitle = "START"
+    @State private var currentLight = CurrentLight.red
+    @State private var redLight = 0.3
+    @State private var yellowLight = 0.3
+    @State private var greenLight = 0.3
+    
+    private func nextColor() {
+        let lightIsOn = 1.0
+        let lightIsOff = 0.2
+        
+        switch currentLight {
+        case .red:
+            greenLight = lightIsOff
+            redLight = lightIsOn
+            currentLight = CurrentLight.yellow
+        case .yellow:
+            redLight = lightIsOff
+            yellowLight = lightIsOn
+            currentLight = CurrentLight.green
+        case .green:
+            yellowLight = lightIsOff
+            greenLight = lightIsOn
+            currentLight = CurrentLight.red
+        
+        }
+    }
+}
+extension ContentView {
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                ColorLights(color: .red, opacity: redLight)
+                ColorLights(color: .yellow, opacity: yellowLight)
+                ColorLights(color: .green, opacity: greenLight)
+                
+                Spacer()
+                StartColorButton(title: buttonTitle) {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    nextColor()
+                }
+            }
             .padding()
+        }
     }
 }
 
--- TrafficLightsApp/StartColorButton.swift
+++ TrafficLightsApp/StartColorButton.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct StartColorButton: View {
+    
+    let title: String
+    let action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .overlay(RoundedRectangle(cornerRadius: 20)
+        .stroke(Color.white, lineWidth: 4))
     }
 }
 
 struct StartColorButton_Previews: PreviewProvider {
     static var previews: some View {
-        StartColorButton()
+        StartColorButton(title: "START", action: {})
     }
 }
