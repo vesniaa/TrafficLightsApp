152ea10df02ca157877d6f5ee71eafb2d284b7f5
Eugenia <pyssiki@yandex.ru>
2022-05-13 15:19:29 +0500

    Initial Commit

---
+++ TrafficLightsApp/TrafficLightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsAppApp.swift
+//  TrafficLightsApp
+//
+//  Created by Евгения Аникина on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
